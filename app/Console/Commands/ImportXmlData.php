0b360bfe352c4d7037670ce8bf36ce31ece586b2
damgbi <danillocsengenharia@outlook.com>
2026-03-18 21:04:03 -0300

    Implementa correçções ao arquivo `ImportXmlData.php`.

--- app/Console/Commands/ImportXmlData.php
+++ app/Console/Commands/ImportXmlData.php
@@ -46,12 +46,18 @@ class ImportXmlData extends Command
 
         $roomsXml = simplexml_load_file(storage_path('xml/rooms.xml'));
 
-        foreach ($roomsXml->room as $room) {   
+        foreach ($roomsXml->room as $room) {  
+            $hotel = Hotel::where('external_id', (string) $room['hotel_id'])->first();
+
+            if (!$hotel) {
+                $this->warn('Hotel não encontrado para quarto XML ID: ' . (string) $room['id']);
+                continue;
+            } 
 
             Room::updateOrCreate(
                 ['external_id' => (string) $room['id']], 
                 [
-                'hotel_id' => (string) $room['hotel_id'],
+                'hotel_id' => $hotel->id,
                 'name' => (string) $room,
                 'inventory_count' => (int) $room['inventory_count'],
                 ]
@@ -65,23 +71,45 @@ class ImportXmlData extends Command
         $reservationsXml = simplexml_load_file(storage_path('xml/reservations.xml'));
         foreach ($reservationsXml->reservation as $reservation) {
 
-            $rooms = $reservation->rooms->room ?? [];
+            $roomXml = $reservation->room ?? null;
 
-            foreach ($rooms as $room) {
+            if (!$roomXml) {
+                 continue;
+            }
 
-                Reservation::create([
+            $hotel = Hotel::where('external_id', (string) $reservation->hotel_id)->first();
+
+            if (!$hotel) {
+                $this->warn('Hotel não encontrado para reserva XML ID: ' . (string) $reservation->id);
+                continue;
+            }
+
+            $room = Room::where('external_id', (string) $roomXml->id)
+            ->where('hotel_id', $hotel->id)
+            ->first();
+
+            if (!$room) {
+                $this->warn('Quarto não encontrado para reserva XML ID: ' . (string) $reservation->id);
+                continue;
+            }
+
+
+            Reservation::updateOrCreate(
+                [
                     'external_id' => (string) $reservation->id,
-                    'hotel_id' => (string) $reservation->hotel_id,
-                    'room_id' => (string) $reservation->room_id,
+                ],
+                [
+                    'hotel_id' => $hotel->id,
+                    'room_id' => $room->id,
                     'customer_first_name' => (string) $reservation->customer->first_name,
                     'customer_last_name' => (string) $reservation->customer->last_name,
-                    'arrival_date' => (string) $reservation->arrival_date,
-                    'departure_date' => (string) $reservation->departure_date,
-                    'total_price' => (float) $reservation->totalprice ?? 0
-                ]);
-            }
-            
+                    'arrival_date' => (string) $roomXml->arrival_date,
+                    'departure_date' => (string) $roomXml->departure_date,
+                    'total_price' => (float) ($roomXml->totalprice ?? 0),
+                ]
+            );
         }
+
 
         $this->info('Reservas importadas com sucesso!');
     }
