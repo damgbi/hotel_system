1c0f3cd0ae7e3ae0e9bdab38b3bf707480bfbd8e
damgbi <danillocsengenharia@outlook.com>
2026-05-20 20:29:46 -0300

    adds news routes and a new view.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.',
+], function() {
+    Route::get('hotels', function() {
+        return view('admin.hotels');
+    })->name('hotels');
+
+    Route::get('reservations', function() {
+        return view('admin.reservations');
+    })->name('reservations');
+
+    Route::get('rooms', function() {
+        return view('admin.rooms');
+    })->name('rooms');
+});
+
