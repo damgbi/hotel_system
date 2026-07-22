c4ceab7eb8685dc1ad9eeab9b2ab09168be812f9
damgbi <danillocsengenharia@outlook.com>
2026-07-22 19:35:11 -0300

    Feat: cria as regras para o sanctum autenticar rotas.

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use App\Http\Controllers\ReservationController;
 
 Route::apiResource('rooms', RoomController::class);
 Route::apiResource('reservations', ReservationController::class);
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+ 
+    return ['token' => $token->plainTextToken];
+});
